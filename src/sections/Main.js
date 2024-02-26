06dd9cf769ae1dae1bd8f50b3d56b7302a9aaddf
Assylzhan <asekabtw@gmail.com>
2024-02-26 12:58:32 +0100

    finish styling navbar

--- src/sections/Main.js
+++ src/sections/Main.js
@@ -17,14 +17,23 @@ function Nav() {
     return () => window.removeEventListener("scroll", transitionNavBar);
   }, []);
 
+  function Link({ title, path }) {
+    return (
+      <li className="nav__link">
+        <a href={path}>{title}</a>
+      </li>
+    );
+  }
+
   return (
     <nav className={`nav ${height && "nav__shadow"}`}>
       <div className="nav-container">
         <h3 className="nav__logo">dev.aso</h3>
         <ul className="nav__links">
-          <li className="nav__link">
-            <a href="">Home</a>
-          </li>
+          <Link title="Home" path="" />
+          <Link title="About" path="" />
+          <Link title="Projects" path="" />
+          <Link title="Contact" path="" />
         </ul>
       </div>
     </nav>
@@ -36,70 +45,6 @@ function Main() {
     <div className="main">
       <div className="main-container">
         <Nav />
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
-        <h1>TEST</h1>
       </div>
     </div>
   );

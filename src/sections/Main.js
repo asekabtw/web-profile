a7d3801864ba979aad4c430e5cfd8efaa6c6c66a
Assylzhan <asekabtw@gmail.com>
2024-02-28 18:40:03 +0100

    import img and style it

--- src/sections/Main.js
+++ src/sections/Main.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./Main.scss";
+import Profile from "../images/Profile.png";
 
 function Main() {
   return (
@@ -13,19 +14,21 @@ function Main() {
 function Hero() {
   return (
     <div className="hero">
-      <div className="hero-text">
-        <h1 className="hero__title">Front-End React Developer</h1>
-        <p className="hero__desc">
-          Hi, I'm Turgunbay Assylzhan. A passionate Front-end React Developer
-          based in Shymkent, Kazakhstan. 📍
-        </p>
-        <span className="hero__links">
-          <a className="hero__link" href=""></a>
-          <a className="hero__link" href=""></a>
-        </span>
-      </div>
-      <div className="hero__img">
-        {/* <img src="../images/picture.jpg" alt="" /> */}
+      <div className="hero-info">
+        <div className="hero-text">
+          <h1 className="hero__title">Front-End React Developer</h1>
+          <p className="hero__desc">
+            Hi, I'm Turgunbay Assylzhan. A passionate Front-end React Developer
+            based in Shymkent, Kazakhstan. 📍
+          </p>
+          <span className="hero__links">
+            <a className="hero__link" href=""></a>
+            <a className="hero__link" href=""></a>
+          </span>
+        </div>
+        <div className="hero__img">
+          <img src={Profile} alt="" />
+        </div>
       </div>
     </div>
   );
